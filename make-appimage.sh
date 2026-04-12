ce1d83226093ec7fa209eb90feafd272219a3563
Link4Electronics <link4electronics@gmail.com>
2026-04-11 22:15:57 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 VERSION=$(pacman -Q openrct2 | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=https://github.com/OpenRCT2/OpenRCT2/raw/refs/heads/develop/resources/logo/icon_flag.svg
 export DESKTOP=/usr/share/applications/io.openrct2.openrct2.desktop

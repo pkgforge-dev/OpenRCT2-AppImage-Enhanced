09f862fdeb4296ffb3d3623c37adcc65452fc130
Link4Electronics <link4electronics@gmail.com>
2026-04-11 22:15:57 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,10 +3,10 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q openrct2 | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q openrct2 | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=https://github.com/OpenRCT2/OpenRCT2/raw/refs/heads/develop/resources/logo/icon_flag.svg
 export DESKTOP=/usr/share/applications/io.openrct2.openrct2.desktop
@@ -16,11 +16,9 @@ export DEPLOY_OPENGL=1
 # Deploy dependencies
 quick-sharun /usr/bin/openrct2
 
-# Additional changes can be done in between here
-
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
 
-# Test the app for 12 seconds, if the test fails due to the app
-# having issues running in the CI use --simple-test instead
-quick-sharun --test ./dist/*.AppImage
+# Test the app for 12 seconds, if the app normally quits before that time
+# then skip this or check if some flag can be passed that makes it stay open
+quick-sharun --simple-test ./dist/*.AppImage

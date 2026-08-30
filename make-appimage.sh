dc2951aa442be8a9f7c2ef877af1188a4a592330
Link4Electronics <link4electronics@gmail.com>
2026-08-30 03:12:33 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -11,7 +11,7 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export ICON=https://github.com/OpenRCT2/OpenRCT2/raw/refs/heads/develop/resources/logo/icon_flag.svg
 export DESKTOP=/usr/share/applications/io.openrct2.openrct2.desktop
 export STARTUPWMCLASS=openrct2
-export DEPLOY_OPENGL=1
+export USE_HOST_DRIVERS_EXPERIMENTAL=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/openrct2
